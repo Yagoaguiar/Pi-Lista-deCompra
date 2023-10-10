f4e23a49a77bf4917f611a80212fba4387b9cadf
Yago Aguiar <yagoaguiar0203@gmail.com>
2023-10-09 23:13:08 -0300

    alteração nas rotas do código #55

--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import styles from '../styles/Login/Login';
+import Register from './Register';
+import Home from './home';
 
-export default function TelaDeLogin() {
+export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [error, setError] = useState('');
 
   const handleLogin = () => {
+    if (validateFields()) {
+      console.log(`Email: ${email}, Senha: ${senha}`);
+      // preciso colocar a lógica de login depois que estiver concluída. Com firabase ou qualquer outro metódo que faça a validação
+      
+      navigation.navigate('Home');
+    }
+  };
 
-    console.log(`Email: ${email}, Senha: ${senha}`);
+  const validateFields = () => {
+    if (!email || !senha) {
+      setError('Por favor, preencha todos os campos.');
+      return false;
+    }
+    return true;
+  };
+
+  const Register = () => {
+    navigation.navigate('Register');
+  };
+  const Forgot = () => {
+    navigation.navigate('Forgot');
   };
 
   return (
@@ -18,9 +40,11 @@ export default function TelaDeLogin() {
         style={styles.input}
         placeholder="Email"
         placeholderTextColor="white"
+        keyboardType="email-adress"
         onChangeText={(text) => setEmail(text)}
         value={email}
       />
+
       <TextInput
         style={styles.input}
         placeholder="Senha"
@@ -29,9 +53,21 @@ export default function TelaDeLogin() {
         value={senha}
         secureTextEntry={true}
       />
+
+      <TouchableOpacity style={styles.signUpButton} onPress={Forgot}>
+        <Text style={styles.forgotButtonText}>Esqueci senha</Text>
+      </TouchableOpacity>
+
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity style={styles.signUpButton} onPress={Register}>
+        <Text style={styles.registerButtonText}>Novo aqui, cadastre-se</Text>
+      </TouchableOpacity>
+
+
+      
     </View>
   );
 }

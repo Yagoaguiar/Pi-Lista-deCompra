d1c45d92c59e49f6727602dbde13efbc56e7be9a
Yago Aguiar <yagoaguiar0203@gmail.com>
2023-10-10 03:03:47 -0300

    criação de lógica Home #32

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,52 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, FlatList, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import styles from '../styles/Home/Home';
+
 
 
 const Home = () => {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [categoryIndex, setCategoryIndex] = useState(0);
+  const categories = ['Mercado', 'Feira', 'Outros'];
 
+  const addTask = () => {
+    if (task) {
+      setTasks([...tasks, { id: Date.now(), text: task, category: categories[categoryIndex], completed: false }]);
+      setTask('');
+    }
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((item) => item.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    const updatedTasks = tasks.map((item) => {
+      if (item.id === taskId) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const changeCategory = (direction) => {
+    if (direction === 'next') {
+      setCategoryIndex((prevIndex) => (prevIndex + 1) % categories.length);
+    } else if (direction === 'prev') {
+      setCategoryIndex((prevIndex) => (prevIndex - 1 + categories.length) % categories.length);
+    }
+  };
 
   return (
     <View>
       <View>
         <TouchableOpacity>
-          <AntDesign/>
+          <AntDesign />
         </TouchableOpacity>
-       
+
         <TouchableOpacity>
           <AntDesign name="right" size={24} color="black" />
         </TouchableOpacity>
@@ -25,16 +58,16 @@ const Home = () => {
           onChangeText={(text) => setTask(text)}
         />
       </View>
-  
-          <View >
-            <TouchableOpacity onPress={() => toggleTaskCompletion(item.id)}>
-            </TouchableOpacity>
-            <Text>{item.text}</Text>
-            <TouchableOpacity onPress={() => deleteTask(item.id)}>
-              <AntDesign/>
-            </TouchableOpacity>
-          </View>
-        
+
+      <View >
+        <TouchableOpacity onPress={() => toggleTaskCompletion(item.id)}>
+        </TouchableOpacity>
+        <Text>{item.text}</Text>
+        <TouchableOpacity onPress={() => deleteTask(item.id)}>
+          <AntDesign />
+        </TouchableOpacity>
+      </View>
+
       <TouchableOpacity
       >
         <AntDesign />
